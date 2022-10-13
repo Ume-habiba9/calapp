61841052dc87a87d3ae2cd2fdc4a6650499746c3
Ume Habiba <habibaume571@gmail.com>
2022-10-13 03:34:34 -0700

    "Styling added"

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
 
   const clicked = (e) => {
     const set = display.concat(e.target.name);
-    // console.log(set)
     setdisplay(set);
+
   }
   function clear() {
     setdisplay('');
@@ -29,42 +29,40 @@ function App() {
     localStorage.setItem("calculation", result);
     setdisplay(result);
   }
-  // function clicked(e) {
-  //   const set = setdisplay(display.concat(e.target.name));
-  //   console.log(set)
-  // }
+
   return (
     <div className="App">
 
       <div className='buttons'>
         <input className='result' value={display} placeholder='0' />
         <div className='row1'>
-          <button name='C' onClick={() => clear()}>C</button>
-          <button name='Del' onClick={() => del()}>Del</button>
-          <button name='.' onClick={clicked}>.</button>
-          <button name='/' onClick={clicked}>/</button>
+          <button name='C' className='btn' onClick={() => clear()}>C</button>
+          <button name='Del' className='btn' onClick={() => del()}>Del</button>
+          <button name='.' className='btn' onClick={clicked}>.</button>
+          <button name='/' className='right' onClick={clicked}>/</button>
         </div>
         <div className='row2'>
-          <button name='1' onClick={clicked}>1</button>
-          <button name='2' onClick={clicked}>2</button>
-          <button name='3' onClick={clicked}>3</button>
-          <button name='*' onClick={clicked}>*</button>
+          <button name='1' className='btn' onClick={clicked}>1</button>
+          <button name='2' className='btn' onClick={clicked}>2</button>
+          <button name='3' className='btn' onClick={clicked}>3</button>
+          <button name='*' className='right' onClick={clicked}>*</button>
         </div>
         <div className='row3'>
-          <button name='4' onClick={clicked}>4</button>
-          <button name='5' onClick={clicked}>5</button>
-          <button name='6' onClick={clicked}>6</button>
-          <button name='+' onClick={clicked}>+</button>
+          <button name='4' className='btn' onClick={clicked}>4</button>
+          <button name='5' className='btn' onClick={clicked}>5</button>
+          <button name='6' className='btn' onClick={clicked}>6</button>
+          <button name='+' className='right' onClick={clicked}>+</button>
         </div>
         <div className='row4'>
-          <button name='7' onClick={clicked}>7</button>
-          <button name='8' onClick={clicked}>8</button>
-          <button name='9' onClick={clicked}>9</button>
-          <button name='-' onClick={clicked}>-</button>
+          <button name='7' className='btn' onClick={clicked}>7</button>
+          <button name='8' className='btn' onClick={clicked}>8</button>
+          <button name='9' className='btn' onClick={clicked}>9</button>
+          <button className='right' name='-' onClick={clicked}>-</button>
         </div>
         <div className='row5'>
-          <button name='0' onClick={clicked}>0</button>
-          <button name='=' onClick={() => cal()}>=</button>
+          <button name='0' id='zero' className='btn' onClick={clicked}>0</button>
+          <button name='=' id='equal' className='btn' onClick={() => cal()}>=</button>
+          <button name='%' className='right' onClick={clicked}>%</button>
         </div>
       </div>
     </div>
